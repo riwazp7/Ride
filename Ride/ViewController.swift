3c0defd0cae7eeb21427b6446fd3d530a3cec765
Riwaz Poudyal <rp7@williams.edu>
2018-01-25 20:56:11 -0500

    init

---
+++ Ride/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ride
+//
+//  Created by Riwaz Poudyal on 1/25/18.
+//  Copyright © 2018 KR. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
