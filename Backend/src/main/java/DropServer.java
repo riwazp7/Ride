1136778b98fd7c7b0b4bd58c305f13563daccd6c
Riwaz Poudyal <rp7@williams.edu>
2018-02-17 13:09:29 -0500

    setup logging

--- Backend/src/main/java/DropServer.java
+++ Backend/src/main/java/DropServer.java
@@ -1,3 +1,4 @@
+import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -5,11 +6,16 @@ public class DropServer {
 
     private final static Logger logger = LoggerFactory.getLogger(DropServer.class.getSimpleName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        // log4j basic configuration; replace later
+        BasicConfigurator.configure();
+
         while(true) {
             try {
                 DropServer.newServer().start();
                 logger.info("Started Drop Server");
+                Thread.sleep(Long.MAX_VALUE);
             } catch (RuntimeException e) {
                 logger.error("Fatal: Application stopped with an error", e);
             }
