ce4e806cc85045b9d1b16242dcde52008bbb6154
Riwaz Poudyal <rp7@williams.edu>
2018-02-25 19:52:44 -0500

    minor

--- Backend/src/main/java/booking/BookingsUtil.java
+++ Backend/src/main/java/booking/BookingsUtil.java
@@ -7,11 +7,4 @@ public class BookingsUtil {
     public static String generateRandomBookingID() {
         return RandomStringUtils.random(6, "0126789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     }
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(generateRandomBookingID());
-        }
-    }
-
 }
