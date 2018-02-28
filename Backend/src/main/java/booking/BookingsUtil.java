10166f018ac61ff3ab00cf03eaa7536846937e55
Riwaz Poudyal <rp7@williams.edu>
2018-02-28 00:13:04 -0500

    method to validate booking request in the server side

--- Backend/src/main/java/booking/BookingsUtil.java
+++ Backend/src/main/java/booking/BookingsUtil.java
@@ -5,13 +5,26 @@ import org.apache.commons.lang.RandomStringUtils;
 
 public class BookingsUtil {
 
+    // Display message in the ui to call bob instead for rides with larger # of riders.
+    private static final int MAX_RIDERS = 5;
+
     public static String generateRandomBookingID() {
         return RandomStringUtils.random(6, "0126789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     }
 
-    public static boolean validateBooking(Booking booking) {
-        if (Strings.isNullOrEmpty(booking.getBookingID())
-                || Strings.isNullOrEmpty(booking.getEmail())) {
+    // Better way to do this?
+    // This should be handled in the client side but the check is still needed to not crash the server.
+    public static boolean validateBookingRequest(BookingRequest bookingRequest) {
+        if (Strings.isNullOrEmpty(bookingRequest.getEmail())
+                || bookingRequest.getNumRiders() > MAX_RIDERS
+                || Strings.isNullOrEmpty(bookingRequest.getEmail())
+                || Strings.isNullOrEmpty(bookingRequest.getName())
+                || Strings.isNullOrEmpty(bookingRequest.getEmail())
+                || Strings.isNullOrEmpty(bookingRequest.getPhone())
+                || Strings.isNullOrEmpty(bookingRequest.getDestination())
+                || Strings.isNullOrEmpty(bookingRequest.getOrigin())
+                || Strings.isNullOrEmpty(bookingRequest.getPrice())) {
+            // Check date for validity.
             return false;
         }
         return true;
