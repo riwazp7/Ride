012d9147b383ece3a0e12f1355a486918d11bad3
Riwaz Poudyal <rp7@williams.edu>
2018-03-07 11:23:00 -0500

    testing sending emails

--- Backend/src/main/java/CommunicationHandler.java
+++ Backend/src/main/java/CommunicationHandler.java
@@ -1,6 +1,15 @@
 import booking.Booking;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
 
 public class CommunicationHandler {
+
+    private static final String EMAIL_HOST = "smtp.googlemail.com";
+    private static final int SMTP_PORT = 465;
+    private static final String EMAIL_FROM = ""; // is this needed?
+    private static final String USER_NAME = "dropwebsitetest1";
+    private static final String PASSWORD = "testtest!1"; // lol
 
     public void handleBookingConfirmation(Booking booking) {
 
@@ -8,6 +17,19 @@ public class CommunicationHandler {
 
     public void handleBookingRequestSuccessful(Booking booking) {
 
+    }
+
+    public static void main(String[] args) throws Exception {
+        Email email = new SimpleEmail();
+        email.setSmtpPort(465);
+        email.setHostName(EMAIL_HOST);
+        email.setAuthenticator(new DefaultAuthenticator(USER_NAME, PASSWORD));
+        email.setSSLOnConnect(true);
+        email.setFrom(EMAIL_FROM);
+        email.setSubject("TestMail");
+        email.setMsg("This is a test mail ... :-)");
+        email.addTo("rp7@williams.edu");
+        email.send();
     }
 
 }
--- Backend/src/main/java/booking/BookingResponse.java
+++ Backend/src/main/java/booking/BookingResponse.java
@@ -2,3 +2,4 @@ package booking;
 
 public class BookingResponse {
 }
+
