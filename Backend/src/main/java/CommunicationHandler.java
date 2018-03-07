ce90bea9ce655eda29935c1eaa28ad365abaece7
Riwaz Poudyal <rp7@williams.edu>
2018-03-07 00:54:27 -0500

    skeleton for communication handler

---
+++ Backend/src/main/java/CommunicationHandler.java
@@ -0,0 +1,13 @@
+import booking.Booking;
+
+public class CommunicationHandler {
+
+    public void handleBookingConfirmation(Booking booking) {
+
+    }
+
+    public void handleBookingRequestSuccessful(Booking booking) {
+
+    }
+
+}
